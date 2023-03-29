e097c62a9b2bda98a038a9ae0242d0a45572e1cf
artem_levin <tema.levin2016@gmail.com>
2023-03-29 19:35:47 +0300

    end commit

--- Program.cs
+++ Program.cs
@@ -31,7 +31,7 @@ string[] CreateWritteArray(int size)
 {
    int count = 0;
    string[] WritteArray = new string[size];
-   for (int i = 0; i <= size; i++)
+   for (int i = 0; i < size; i++)
    {
       WritteArray[i] = Console.ReadLine();
       if (WritteArray[i].Length <= 3) count++;
@@ -48,7 +48,9 @@ string[] CreateWritteArray(int size)
    }
    return EndArray;
 }
-
+/// <summary>
+/// Выводит массив на экран
+/// </summary>
 void ShowArray(string[] MyArray)
 {
    Console.Write("Строки содержащие 3 или меньше символов:");
@@ -57,6 +59,28 @@ void ShowArray(string[] MyArray)
       if (MyArray.Length == i + 1) Console.Write($"{MyArray[i]}. ");
       else Console.Write($"{MyArray[i]}, ");
    }
+}
+
+
+Console.Write("Если вы хотите использовать готовый массив введите 'Y', если хотите ввести свой введите 'N': ");
+string solution = Console.ReadLine();
+if (solution.ToLower() == "y")
+{
+   string[] MyArray = CreateRandomArray("y");
+   ShowArray(MyArray);
+}
+
+if (solution.ToLower() == "n")
+{
+   Console.Write("Введите желаемый размер массива: ");
+   int size = Convert.ToInt32(Console.ReadLine());
+   string[] MyArray = CreateWritteArray(size);
+   ShowArray(MyArray);
+}
+if (solution.ToLower() != "n" & solution.ToLower() != "y")
+{
+   Console.WriteLine("Вы ввели некорректный ответ.");
+   return;
 }
 
 
