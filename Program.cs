532cf79ba43be85b58c52262e1d3b28bf399f6f2
artem_levin <tema.levin2016@gmail.com>
2023-03-29 19:02:02 +0300

    Creat 'CreateWriteArray'

--- Program.cs
+++ Program.cs
@@ -4,21 +4,49 @@
 /// <returns> Возвращает готовый массив</returns>
 string[] CreateRandomArray(string YesOrNo)
 {
-   string[] WordList = { "круглосуточный", "3", "трех-звенный", "раз", "cam", "взаимный", "56", "три", "умный", "шесть", "фронтенд", "ещё", "уполномоченный", "что", "нил", "фирменный", "сетевой", "мы", "сфокусированный", "портал", "вуз", "уж" };
+   string[] WordList = { "круглосуточный", "3", "трех-звенный", "OK", "раз", "cam", "взаимный", "56", "три", "умный", "шесть", "фронтенд", "ещё", "уполномоченный", "что", "нил", "фирменный", "сетевой", "мы", "сфокусированный", "портал", "вуз", "уж" };
    int count = 0;
    for (int i = 0; i < WordList.Length; i++)
    {
       if (WordList[i].Length <= 3) count++;
    }
-   string[] EndList = new string[count];
+   string[] EndArray = new string[count];
    count = 0;
    for (int i = 0; i < WordList.Length; i++)
    {
       if (WordList[i].Length <= 3)
       {
-         EndList[count] = WordList[i];
+         EndArray[count] = WordList[i];
          count++;
       }
    }
-   return EndList;
+   return EndArray;
 }
+/// <summary>
+/// Создает массив с символами <= 3 из массива, который вводится с клавиатуры
+/// </summary>
+/// <param name="size">размер массива</param>
+/// <returns></returns>
+string[] CreateWritteArray(int size)
+{
+   int count = 0;
+   string[] WritteArray = new string[size];
+   for (int i = 0; i <= size; i++)
+   {
+      WritteArray[i] = Console.ReadLine();
+      if (WritteArray[i].Length <= 3) count++;
+   }
+   string[] EndArray = new string[count];
+   count = 0;
+   for (int i = 0; i < WritteArray.Length; i++)
+   {
+      if (WritteArray[i].Length <= 3)
+      {
+         EndArray[count] = WritteArray[i];
+         count++;
+      }
+   }
+   return EndArray;
+}
+
+
